209b732864e3a7989e04cd34e75007ef92f0319e
Professor Rodrigo Bossini <professorbossini@gmail.com>
2021-03-27 00:01:46 -0300

    Trabalhamos no mss de consulta. Retomar dai

--- consulta/index.js
+++ consulta/index.js
@@ -2,12 +2,26 @@ const express = require ('express');
 const app = express();
 app.use (express.json());
 
-app.get('/lembretes', (req, res) => {
+const funcoes = {
+  LembreteCriado: (lembrete) => {
+    baseConsulta[lembrete.contador] = lembrete;
+  },
+  ObservacaoCriada: (observacao) => {
+    const observacoes = baseConsulta[observacao.lembreteId]['observacoes'] || [];
+    observacoes.push(observacao);
+    baseConsulta[observacao.lembreteId]['observacoes'] = observacoes;
+  }
+};
 
+const baseConsulta = {};
+
+app.get('/lembretes', (req, res) => {
+  res.status(200).send(baseConsulta);
 });
 
 app.post('/eventos', (req, res) => {
-
+  funcoes[req.body.tipo](req.body.dados);
+  res.status(200).send(baseConsulta);
 });
 
 app.listen(6000, () => console.log ("Microsserviço consulta. Porta 6000."));
